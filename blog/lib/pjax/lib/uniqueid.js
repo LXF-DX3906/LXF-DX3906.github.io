6810d6de15a8dac1eacc22449399a0d0b79df741
LXF-DX3906 <1621413639@qq.com>
2024-12-26 22:53:30 +0800

    Site updated: 2024-12-26 22:53:28

--- blog/lib/pjax/lib/uniqueid.js
+++ blog/lib/pjax/lib/uniqueid.js
@@ -1,8 +1,1 @@
-module.exports = (function() {
-  var counter = 0;
-  return function() {
-    var id = "pjax" + new Date().getTime() + "_" + counter;
-    counter++;
-    return id;
-  };
-})();
+module.exports=(()=>{var r=0;return function(){var e="pjax"+(new Date).getTime()+"_"+r;return r++,e}})();
